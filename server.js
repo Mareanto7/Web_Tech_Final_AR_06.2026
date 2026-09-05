99b8bb45149e57c7f61e455e52ba3c8f9491ce3f
Antonio Rinaldi <161433173+Mareanto7@users.noreply.github.com>
2026-09-05 02:16:20 +0200

    04.09.2026 - Draft bookings CRUD

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 // The adapter holds the Postgres connection, the client uses the adapter to run typed queries, and the URL comes from the environment so no secret is hardocded
 require('dotenv').config();
 const { PrismaPg } = require('@prisma/adapter-pg');
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient, Channel } = require('@prisma/client');
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
@@ -44,7 +44,27 @@ app.get('/register', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-  res.render('login', {error: null});
+  res.render('login', { error: null });
+});
+
+app.post('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      // If there's an error, send them back to properties
+      res.clearCookie('connect.sid');
+      return res.redirect('/properties');
+    }
+    // Clean delete and redirect
+    res.clearCookie('connect.sid');
+    return res.redirect('/login');
+  });
+});
+
+app.get('/', (req, res) => {
+  if( req.session.userId ){
+    return res.redirect('/properties');
+  }
+  res.redirect('/login');
 });
 
 app.post('/login', async (req, res) => {
@@ -97,7 +117,7 @@ app.post('/register', async (req, res) => {
 });
 
 app.get('/properties', requireAuth, async (req, res) => {
-  const properties = await prisma.property.findMany();
+  const properties = await prisma.property.findMany({ where: { isActive : true} });
   res.render('properties', {properties: properties});
 });
 
@@ -126,6 +146,22 @@ app.post('/properties/new', requireAuth, async (req, res) => {
   return res.redirect('/properties');
 });
 
+app.get('/properties/mine', requireAuth, async (req, res) => {
+  // Fetch it all applicable properties for the user
+  const properties = await prisma.property.findMany({ where: { ownerId: req.session.userId }});
+
+  // Return the myProperties page
+  return res.render('myProperties', { properties: properties, error: null });
+
+});
+
+app.get('/bookings/mine', requireAuth, async (req, res) => {
+  const bookings = await prisma.booking.findMany({ where: { guestId: req.session.userId }, include: { property: true }});
+
+  // Render the booking page list
+  return res.render('myBookings', { bookings: bookings, error: null });
+});
+
 app.get('/properties/:id/edit', requireAuth, async (req, res) => {
   const id = parseInt(req.params.id);
   // Fetch it to check the ownership
@@ -136,7 +172,7 @@ app.get('/properties/:id/edit', requireAuth, async (req, res) => {
     return res.redirect('/properties'); // silently refuse
   }
 
-  res.render('editProperty', { property:property, error: null});
+  res.render('editProperty', { property: property, error: null});
 });
 
 
@@ -188,6 +224,126 @@ app.post('/properties/:id/delete', requireAuth, async (req, res) => {
   // Back to the list / home
   return res.redirect('/properties');
 
+});
+
+app.post('/properties/:id/publish', requireAuth, async (req, res) => {
+  // We get which property from the URL - but comes as text so we need to parse it
+  const id = parseInt(req.params.id);
+  // Fetch it to check the ownership
+  const property = await prisma.property.findUnique({ where: { id: id }})
+
+  // Ownership guard - must exists AND must belong to the logged-in user
+  if (!property || property.ownerId !== req.session.userId){
+    return res.redirect('/properties'); // silently refuse
+  }
+  // Publish it
+  await prisma.property.update({ where: { id: id }, data: { isActive: true }});
+
+  // Back to the list / home
+  return res.redirect('/properties');
+});
+
+app.post('/properties/:id/unpublish', requireAuth, async (req, res) => {
+  // We get which property from the URL - but comes as text so we need to parse it
+  const id = parseInt(req.params.id);
+  // Fetch it to check the ownership
+  const property = await prisma.property.findUnique({ where: { id: id }})
+
+  // Ownership guard - must exists AND must belong to the logged-in user
+  if (!property || property.ownerId !== req.session.userId){
+    return res.redirect('/properties'); // silently refuse
+  }
+  // Unpublish it
+  await prisma.property.update({ where: { id: id }, data: { isActive: false }});
+
+  // Back to the list / home
+  return res.redirect('/properties');
+});
+
+app.get('/properties/:id/book', requireAuth, async (req, res) => {
+  const id = parseInt(req.params.id);
+  // Fetch it to check the ownership
+  const property = await prisma.property.findUnique({ where: { id: id }})
+
+  // Ownership guard - must exists AND must belong to the logged-in user
+  if ( !property || property.ownerId === req.session.userId || !property.isActive ){
+    return res.redirect('/properties');
+  }
+
+  // Render the new booking
+  return res.render('newBooking', { property: property, error: null });
+});
+
+
+app.post('/properties/:id/book', requireAuth, async (req, res) => {
+  // Property guard - only we know, from the URL, the correct property ID
+  const id= parseInt(req.params.id);
+
+  // Fetch it to check the ownership
+  const property = await prisma.property.findUnique({ where: { id: id }})
+
+  // Let's pull data from the form
+  const checkInDate = new Date(req.body.checkInDate);
+  const checkOutDate = new Date(req.body.checkOutDate);
+  const channel = req.body.channel;
+  const numberGuests = parseInt(req.body.numberGuests);
+  const guestId = req.session.userId;
+
+  // Ownership guard - must exists AND must belong to the logged-in user
+  if ( !property || property.ownerId === req.session.userId || !property.isActive ){
+    return res.redirect('/properties');
+  }
+
+  // Validate days
+  if(isNaN(checkInDate.getTime()) || isNaN(checkOutDate.getTime())){
+    return res.render('newBooking', { property: property, error: 'Please insert a valid time '});
+  }
+
+  // Check the nights are valid
+  const todayMidnight = new Date();
+  todayMidnight.setUTCHours(0, 0, 0, 0);
+  if(checkOutDate <= checkInDate || checkInDate < todayMidnight){
+    return res.render('newBooking', { property: property, error: 'No check-in after check-out or past check-in are allowed, correct before proceeding!'});
+  }
+
+  // Calculate days and Compute stay
+  const MS_PER_DAY = 1000 * 60 * 60 * 24;
+  const nights = (checkOutDate - checkInDate) / MS_PER_DAY;
+
+  // Check the nights are valid
+  if(numberGuests > property.maxGuests || numberGuests < 1 || isNaN(numberGuests)){
+    return res.render('newBooking', { property: property, error: `Include a number of guests between 1 and ${property.maxGuests} before proceeding!`})
+  }
+
+  // Check the channel
+  if (!Object.values(Channel).includes(channel)) {
+    return res.render('newBooking', { property: property, error: 'Invalid booking channel' });
+}
+
+  // Compute booking price
+  const bookingPrice = property.price.mul(nights);
+
+  // Create the booking object
+  await prisma.booking.create({ data: { guestId: guestId, propertyId: id, checkInDate: checkInDate, checkOutDate: checkOutDate, channel: channel, bookingPrice: bookingPrice, numberGuests: numberGuests }});
+  // Redirects to properties listing
+  return res.redirect('/properties');
+});
+
+app.post('/bookings/:id/cancel', requireAuth, async (req, res) => {
+  const id = parseInt(req.params.id);
+  // Fetch it to check the ownership
+  const booking = await prisma.booking.findUnique({ where: { id: id }})
+
+  // Ownership guard - must exists AND must belong to the logged-in user
+  if ( !booking || booking.guestId !== req.session.userId ){
+    return res.redirect('/bookings/mine');
+  }
+
+  // Cancel it and save
+  await prisma.booking.update({ where: { id: id }, data: { status: 'CANCELLED' }});
+
+  // Redirect to the list of my bookings
+  return res.redirect('/bookings/mine');
 });
 
 app.listen(PORT, () => console.log(`Express started on port ${PORT}`));
